1455d43440f42f3de8eb667fd9bf67c820fb994a
yuvraj-singh-codes <yuvraj.excel2011@gmail.com>
2024-12-16 12:50:36 +0100

    few things changed for better performance

---
+++ README.md
@@ -0,0 +1,27 @@
+
+<h1 align="center">LUNA Service Request System</h1>
+
+The primary objective of this ticketing system is to streamline the project management processes, enhance communication, and improve the overall efficiency of the organization.
+As a company grows it becomes increasingly challenging to manage numerous projects effectively. The proposed ticketing system will serve as a centralized management tool that processes and catalogs customer service requests, tracks project progress and allocates work among the management, technical, and functional teams.
+The ticketing system will enable seamless collaboration between teams by facilitating the assignment and tracking of tasks, communication through internal notes, and efficient handling of client requirements and project changes. Furthermore, the system will incorporate AI-driven data analytics to optimize ticket assignments, analyze project costs, and identify areas for improvement.
+
+
+🚀 I am a passionate and forward-thinking technology enthusiast, specializing in outer space exploration, quantum computing, and artificial intelligence. With a keen interest in the pioneering work by OpenAI, Tesla and SpaceX, I strive to push the boundaries of what's possible and create a better future for humanity.
+
+🖥️ As an aspiring software engineer, I have extensive experience in programming languages such as C++, Java, Python, SQL, Angular, Ionic, JavaScript, and TypeScript. My computer literacy and technical expertise enable me to develop innovative solutions for complex problems in the rapidly evolving world of technology.
+
+🌌 My fascination for outer space and quantum computing drives me to stay updated with the latest advancements in these fields, inspiring me to take on new challenges and expand my skill set.
+
+🤖 As a strong advocate for artificial intelligence, I am amazed at what OpenAI has achieved and eagerly await the opportunity to contribute to their groundbreaking research and applications. Through collaboration and continuous learning, I believe that we can revolutionise the world, making
+
+
+
+💡 My skill set includes a diverse range of programming languages and technologies. I am proficient in Angular, Vue, JavaScript, HTML, CSS, TypeScript, Node.js, PHP, Python, Java, C#, C++, SQL, and MongoDB. This versatility allows me to adapt quickly to different development environments and contribute effectively to various aspects of software engineering.
+
+🎮 Game development has always been an area of immense fascination for me. I enjoy exploring the creative possibilities of blending cutting-edge technology with captivating storytelling to create immersive experiences. Through personal projects and coursework, I have gained hands-on experience in game development frameworks, engines, and design principles, which has further fueled my passion for this field.
+
+🤖 In addition to game development, I am deeply interested in artificial intelligence (AI). The potential of AI to revolutionize industries and drive innovation is truly remarkable. I am eager to apply my programming skills and knowledge to contribute to the development of intelligent systems and explore the possibilities of machine learning and data analysis.
+
+🌟 As a proactive learner, I am always seeking opportunities to expand my knowledge and stay up-to-date with the latest advancements in the tech industry. I am a firm believer in continuous improvement and enjoy collaborating with like-minded individuals who share a passion for technology and innovation.
+
+🌐 If you're interested in discussing AI, game development, or any exciting tech projects, feel free to connect with me. I'm open to new opportunities, collaborations, and sharing ideas. Let's connect and explore the possibilities together!
---
+++ backend/dist/server.js
@@ -0,0 +1,104 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.server = exports["default"] = void 0;
+var _express = _interopRequireDefault(require("express"));
+var _cors = _interopRequireDefault(require("cors"));
+var _dotenv = _interopRequireDefault(require("dotenv"));
+var _testTicket = _interopRequireDefault(require("./test_routers/testTicket.router"));
+var _testUser = _interopRequireDefault(require("./test_routers/testUser.router"));
+var _testNotifications = _interopRequireDefault(require("./test_routers/testNotifications.router"));
+var _testGroup = _interopRequireDefault(require("./test_routers/testGroup.router"));
+var _testClient = _interopRequireDefault(require("./test_routers/testClient.router"));
+var _testTicketDB = require("./configs/testTicketDB.config");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+// import { createProxyMiddleware } from 'http-proxy-middleware';
+var proxy = require("express-http-proxy");
+_dotenv["default"].config();
+//test
+
+//test routers
+(0, _testTicketDB.dbConnection)();
+var app = (0, _express["default"])();
+exports["default"] = app;
+app.use((0, _cors["default"])());
+app.use(_express["default"].json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+var ticketUrl;
+var userUrl;
+var groupUrl;
+var notificationUrl;
+var clientUrl;
+if (process.env.NODE_ENV === 'production') {
+  ticketUrl = "https://luna-ticket-service-production.up.railway.app";
+} else {
+  ticketUrl = "http://localhost:3001";
+}
+if (process.env.NODE_ENV === 'production') {
+  userUrl = "https://luna-user-service-production.up.railway.app";
+} else {
+  userUrl = "http://localhost:3002";
+}
+if (process.env.NODE_ENV === 'production') {
+  groupUrl = "https://luna-group-service-production.up.railway.app";
+} else {
+  groupUrl = "http://localhost:3003";
+}
+if (process.env.NODE_ENV === 'production') {
+  notificationUrl = "https://luna-notification-service-production.up.railway.app";
+} else {
+  notificationUrl = "http://localhost:3004";
+}
+if (process.env.NODE_ENV === 'production') {
+  clientUrl = "https://luna-client-service-production.up.railway.app";
+} else {
+  clientUrl = "http://localhost:3005";
+}
+app.use("/api/ticket", proxy(ticketUrl, {
+  proxyReqPathResolver: function proxyReqPathResolver(req) {
+    return "/api/ticket".concat(req.url);
+  }
+}));
+app.use("/api/user", proxy(userUrl, {
+  proxyReqPathResolver: function proxyReqPathResolver(req) {
+    return "/api/user".concat(req.url);
+  }
+}));
+app.use("/api/group", proxy(groupUrl, {
+  proxyReqPathResolver: function proxyReqPathResolver(req) {
+    return "/api/group".concat(req.url);
+  }
+}));
+app.use("/api/notifications", proxy(notificationUrl, {
+  proxyReqPathResolver: function proxyReqPathResolver(req) {
+    return "/api/notifications".concat(req.url);
+  }
+}));
+app.use("/api/client", proxy(clientUrl, {
+  proxyReqPathResolver: function proxyReqPathResolver(req) {
+    return "/api/client".concat(req.url); // Prefix the request path with /api/user
+  }
+}));
+
+//test routers
+app.use('/api/test_ticket', _testTicket["default"]);
+app.use('/api/test_user', _testUser["default"]);
+app.use('/api/test_notifications', _testNotifications["default"]);
+app.use('/api/test_group', _testGroup["default"]);
+app.use('/api/test_client', _testClient["default"]);
+app.get('/api/welcome', function (req, res) {
+  res.status(200).send({
+    message: 'Welcome to the server!'
+  });
+});
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+  console.log("Server running on http://localhost:".concat(port));
+});
+exports.server = server;
